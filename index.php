d6e4199a109d294375a094a493e9d5a250c54519
Leorsousa <leorsousa05@gmail.com>
2022-05-25 16:39:12 -0300

    Bootstrap

--- index.php
+++ index.php
@@ -4,18 +4,21 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title> CRUD com PHP e MySQL </title>
-<link href="css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
 </head>
 <body>
-<div class="container">
+<div class="container border border-primary shadow-lg rounded mt-5">
     
-    <h1>CRUD com PHP e MySQL</h1>
-    <p>Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.</p>
+    <h1 class="display-4 text-center">CRUD com PHP e MySQL</h1>
+    <p class="text-center">Operações de <b>inserção</b>, <b>leitura</b>, <b>atualização</b> e <b>exclusão</b> de dados.</p>
     <hr>
     
-    <a href="fabricantes/listar.php">Fabricantes</a> <br>
-    <a href="produtos/listar.php">Produtos</a>
-    
+    <div class="mx-auto">
+        <a href="fabricantes/listar.php" class="btn btn-primary mt-2 mb-5 mx-auto">Fabricantes</a>
+        <a href="produtos/listar.php" class="btn btn-outline-dark mt-2 mb-5 mx-auto">Produtos</a>
+    </div>
+
 </div>
 
 </body>
--- produtos/listar.php
+++ produtos/listar.php
@@ -8,7 +8,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Produtos | SELECT - CRUD com PHP e MySQL </title>
-<link href="../css/style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
 <body>
 
@@ -20,23 +20,26 @@
 <div class="container">
     
     <h2>Lendo e carregando todos os produtos</h2>
-    <p><a href="inserir.php">Inserir</a></p>  
+    <p><a href="inserir.php" class="btn btn-primary px-4">Inserir</a></p>  
 
     <hr>
 
-
-    <?php foreach($listaDeProdutos as $produto) { ?>
-    <ul>
-        <li><b>Nome:  </b> <?=$produto["nome"] ?>  </li>
-        <li><b>Preço:  </b> <?='R$ '.$produto["preco"] ?>  </li>
-        <li><b>Quantidade:  </b> <?=$produto["quantidade"] ?>  </li>
-        <li><b>Descrição:  </b> <?=$produto["descricao"] ?>  </li>
-        <li><b>Fabricante:  </b> <?=$produto["fabricante"] ?> </li>
-    </ul>
-    <a href="atualizar.php?id=<?=$produto['id']?>">Atualização</a>
-    <a href="excluir.php?id=<?=$produto['id']?>">Excluir</a>
-
-    <?php } ?>
+    <div class="row">
+        <?php foreach($listaDeProdutos as $produto) { ?>
+        <article class="col-sm-6 col-md-4 col-xl-6">
+            <h3><b>Nome:   <?=$produto["nome"] ?>  </b></h3>
+            <p><b>Preço:   <?='R$ '.$produto["preco"] ?>  </b></p>
+            <p><b>Quantidade:   <?=$produto["quantidade"] ?>  </b></p>
+            <p><b>Descrição:   <?=$produto["descricao"] ?> </b> </p>
+            <p><b>Fabricante:   <?=$produto["fabricante"] ?></b> </p>
+            
+            <p>
+                <a href="atualizar.php?id=<?=$produto['id']?>">Atualização</a>
+                <a href="excluir.php?id=<?=$produto['id']?>">Excluir</a>
+            </p>
+        </article>
+        <?php } ?>
+    </div>
 
 </div>
 
